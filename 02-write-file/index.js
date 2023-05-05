41fcb74595c2adcbc9c6a0f3f28687ee0ade89be
Artyom Pizin <artempetr2016@gmail.com>
2023-05-05 17:56:41 +0300

    feat: implement 02-write-file functionality

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout} = process;
+
+const GREETING_MESSAGE = 'Hello! Enter some text into the console to write it to text.txt\n';
+const FAREWELL_MESSAGE = 'Your data is saved to a file text.txt. Bye!\n';
+const EXIT_CODE = 'exit';
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write(GREETING_MESSAGE);
+
+
+stdin.on('data', (data) => {
+  const text = data.toString();
+
+  if (text.trim() === EXIT_CODE) {
+    process.exit();
+  } else {
+    writeStream.write(text);
+  }
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
+
+process.on('exit', () => {
+  stdout.write(FAREWELL_MESSAGE);
+})
