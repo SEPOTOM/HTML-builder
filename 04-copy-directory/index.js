9ebb705a734806f1c386568a95214fbd19ef16d4
Artyom Pizin <artempetr2016@gmail.com>
2023-05-05 19:53:52 +0300

    feat: implement 04-copy-directory functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 
+const FOLDER_NAME = 'files';
+const CLONE_FOLDER_NAME = 'files-copy';
+
+function copyDir() {
+  let sourceFiles = [];
+
+  fs.mkdir(path.join(__dirname, CLONE_FOLDER_NAME), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(path.join(__dirname, FOLDER_NAME), (err, files) => {
+    sourceFiles = files;
+
+    files.forEach((file) => {
+      fs.readFile(path.join(__dirname, FOLDER_NAME, file), 'utf-8', (err, data) => {
+        fs.writeFile(path.join(__dirname, CLONE_FOLDER_NAME, file), data, (err) => {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+
+  fs.readdir(path.join(__dirname, CLONE_FOLDER_NAME), (err, files) => {
+    files.forEach((file) => {
+      if (!sourceFiles.includes(file)) {
+        fs.unlink(path.join(__dirname, CLONE_FOLDER_NAME, file), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+  });
+}
+
+copyDir();
